12710629fd1eb029be8a3f72f27bb01104130462
fr2d9y2 <paG(1015)>
2017-04-06 11:20:51 -0700

    Refactor: one assert per teest function

--- mars-rover/src/rover.py
+++ mars-rover/src/rover.py
@@ -1,5 +1,26 @@
 class Rover:
+
+    global commands
+
     def __init__(self, x, y, orientation):
         self.x = x
         self.y = y
         self.orientation = orientation
+
+    def move(self, commands):
+        self.moveBy(commands[0])
+
+    def moveBy(self, commandKey):
+        command = commands[commandKey]
+        command(self)
+
+    def move_forward(self):
+        self.y += 1
+
+    def move_backward(self):
+        self.y -= 1
+
+    commands = {
+        'f': move_forward,
+        'b': move_backward
+    }
--- mars-rover/test/test_rover.py
+++ mars-rover/test/test_rover.py
@@ -13,3 +13,14 @@ def test_initial_y():
 
 def test_initial_orientation():
     assert_equal('N', r.orientation)
+
+def test_move_f():
+    r.move(list("f"))
+    assert_equal(22, r.y)
+    assert_equal(10, r.x)
+
+@with_setup(setup)
+def test_move_b():
+    r.move(list("b"))
+    assert_equal(10, r.x)
+    assert_equal(20, r.y)
